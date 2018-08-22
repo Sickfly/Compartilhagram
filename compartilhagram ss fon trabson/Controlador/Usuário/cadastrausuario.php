ae0ce43647f3d0419aa361daa88c86d73086aaa5
Sickfly <mateusvasco3@gmail.com>
2018-08-22 15:56:41 -0300

    Finalizaçao

--- compartilhagram ss fon trabson/Controlador/Usuário/cadastrausuario.php
+++ compartilhagram ss fon trabson/Controlador/Usuário/cadastrausuario.php
@@ -6,9 +6,10 @@
 		
 		$request = filter_var_array($request,[ 'nomePróprio' => FILTER_DEFAULT, 'sobrenome' => FILTER_DEFAULT,
 												'email' => FILTER_VALIDATE_EMAIL, 'senha' => FILTER_DEFAULT,
-												'confirmasenha'=> FILTER_DEFAULT, 'alertasEmail' => FILTER_VALIDATE_BOOLEAN,
+												'confirmaSenha'=> FILTER_DEFAULT, 'alertasEmail' => FILTER_VALIDATE_BOOLEAN,
 												'aceitaTermos' => FILTER_VALIDATE_BOOLEAN,
-												'dataNasc' => FILTER_DEFAULT 
+												'dataNasc' => FILTER_DEFAULT,
+												'visibilidadePublicações' => FILTER_VALIDATE_INT
 		]
 		);
 		$dia = $request['dataNasc'];
@@ -18,9 +19,11 @@
 			$subt = $dataa->diff($agr);		
 			$qtdddd = $subt->days;
 			$qtddda = $subt->y;
-		
 			echo "<p> Quantidade de dias $qtdddd de você</p>";
 			echo "<p> Quantidade de anos $qtddda de você</p>";
+			if($qtddda < 16){
+				echo "tu é de menó pilantra, ACESSO NEGADO PÁ VC<br>";
+			}
 			}
 		else{
 			$erros[] = "O formato de data é inválido.";
@@ -28,13 +31,13 @@
 		
 		
 		
-		$nome = $request['nomePróprio'];
-		if ($nome == false){
+		$nome = $request['nomePróprio'] ;
+		if ($nome == false || (strlen($nome) < 3 || strlen($nome) > 35 )){
 			$erros[] = "O nome é inválido.";  
 		}
 
 		$sobrenome = $request['sobrenome'];
-		if ($sobrenome == false){
+		if ($sobrenome == false || (strlen($nome) < 3 || strlen($nome) > 35 )){
 			$erros[] = "O sobrenome é inválido.";  
 		}
 		$email = $request['email'];
@@ -42,22 +45,36 @@
 			$erros[] = "O Email é inválido.";  
 		}
 		$senha = $request['senha'];
-		if ($senha == false){
+		if ($senha == false || (strlen($senha) < 6 || strlen($senha) > 12 )) {
 			$erros[] = "A senha é inválida.";  
 		}
 
-		$confirmasenha = $request['confirmasenha'];
-		if ($confirmasenha == false){
+		$confirmasenha = $request['confirmaSenha'];
+		if ($confirmasenha != $senha){
 			$erros[] = "As senhas estão diferentes seu otário.";  
 		}
+		if ($confirmasenha == false){
+				$erros[] = "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!CONFIRMA ESSA MERDA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";  
+		}
+		
+		
 		$alertasEmail = $request['alertasEmail'];
 		if ($alertasEmail == false){
-			$erros[] = "adljfghqdsfr.";  
+			$erros[] = "ããããããããnnnnnnn, ele não quer receber alerta de email, maior vacilão! EU ACHEI PESADOOO";  
 		}
 		$aceitaTermos = $request['aceitaTermos'];
 		if ($aceitaTermos == false){
 			$erros[] = "Aceite os termos de uso, vacilão.";  
 		}
+		
+		$visi = $request['visibilidadePublicações'];
+		if($visi == FALSE){
+			$erros[]="poiuhygtfd";
+		}
+		else if($visi !=1 && $visi !=2 && $visi !=3){
+			$erros[]="poiuhygtfd";
+		}
+		
 
 	 foreach($erros as $msg){
 		 echo " Uma ameaça foi detectada:  <strong>$msg</strong> <br><br>";
--- compartilhagram ss fon trabson/compartilhagram.php
+++ compartilhagram ss fon trabson/compartilhagram.php
@@ -7,37 +7,36 @@
 
 	<body>
 	
-	<h1>InstaCPII</h1>
+		<h1>InstaCPII</h1>
+		<div>
+			<form method="POST" action="Controlador/Usuário/cadastrausuario.php" novalidate>
+				<input name="nomePróprio" type="text" minlength='3' maxlength='35' placeholder="Nome próprio" required />
+				<input name="sobrenome" type="text" minlength='3' maxlength='35' placeholder="Sobrenome" required /><br/>
 
-	<!-- TODO: Formulário de login -->
+				<input name="email" type="email" placeholder="E-Mail" required /><br/>
 
-	<form method="POST" action="Controlador/Usuário/cadastrausuario.php" novalidate>
-		<input name="nomePróprio" type="text" minlength='3' maxlength='35' placeholder="Nome próprio" required />
-		<input name="sobrenome" type="text" minlength='3' maxlength='35' placeholder="Sobrenome" required /><br/>
+				<input name="senha" type="password" min='6' max='12' placeholder="Senha" required /><br/>
 
-		<input name="email" type="email" placeholder="E-Mail" required /><br/>
+				<input name="confirmaSenha" type="password" min='6' max='12' placeholder="Confirmar senha" required /><br/>
 
-		<input name="senha" type="password" min='6' max='12' placeholder="Senha" required /><br/>
+				<label>Data de nascimento: <input name="dataNasc" type="date" required /></label><br/>
 
-		<input name="confirmaSenha" type="password" min='6' max='12' placeholder="Confirmar senha" required /><br/>
+				<label>
+					Quem pode ver as suas publicações?
+					<select name="visibilidadePublicações">
+						<option value="" disabled selected >Selecione</option>
+						<option value="1">Amigos</option>
+						<option value="2">Amigos de amigos</option>
+						<option value="3">Todos</option>
+					</select>
+				</label><br/>
 
-		<label>Data de nascimento: <input name="dataNasc" type="date" required /></label><br/>
+				<label><input name="alertasEmail" type="checkbox" />Receber alertas por e-mail.</label><br/>
 
-		<label>
-			Quem pode ver as suas publicações?
-			<select name="visibilidadePublicações">
-				<option value="" disabled selected >Selecione</option>
-				<option value="1">Amigos</option>
-				<option value="2">Amigos de amigos</option>
-				<option value="3">Todos</option>
-			</select>
-		</label><br/>
+				<label><input name="aceitaTermos" type="checkbox" required />Li e concordo com os termos de uso e com a política de privacidade.</label><br/>
 
-		<label><input name="alertasEmail" type="checkbox" />Receber alertas por e-mail.</label><br/>
-
-		<label><input name="aceitaTermos" type="checkbox" required />Li e concordo com os termos de uso e com a política de privacidade.</label><br/>
-
-		<input type="submit" value="Cadastrar"/>
-	</form>
+				<input type="submit" value="Cadastrar"/>
+			</form>
+		</div>
 	</body>
 </html>
